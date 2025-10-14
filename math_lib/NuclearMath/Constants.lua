75311d3ff022aa40ca765c74663359919bbb43d0
acaba1806 <acaba1806@users.noreply.github.com>
2025-10-14 16:09:46 -0600

    chore(legal): Apache-2.0 LICENSE/NOTICE (Terranova) + headers + attrs/ignore

--- math_lib/NuclearMath/Constants.lua
+++ math_lib/NuclearMath/Constants.lua
@@ -1,3 +1,10 @@
+-- Copyright 2025 Terranova Incorporated
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+-- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+-- an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+-- specific language governing permissions and limitations under the License.
 local Constants = {}
 
 Constants.Avogadro = 6.02214076e23 -- 1/mol
@@ -29,3 +36,4 @@ Constants.PumpInertia = 12.5 -- kg*m^2 hypothetical
 Constants.PumpDamping = 3.25 -- N*m*s
 
 return Constants
+
--- math_lib/NuclearMath/init.lua
+++ math_lib/NuclearMath/init.lua
@@ -1,3 +1,10 @@
+-- Copyright 2025 Terranova Incorporated
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+-- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+-- an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+-- specific language governing permissions and limitations under the License.
 local Constants = require(script.Constants)
 local CoreKinetics = require(script.CoreKinetics)
 local ThermalHydraulics = require(script.ThermalHydraulics)
@@ -21,3 +28,4 @@ return {
 	CoolantChemistry = CoolantChemistry,
 	Utils = Utils,
 }
+
